3095af6fa04fd9e775b6c1c566ab1f7df7f03ad9
volodymyrzaplatynskii <volodymyr.zaplatynskyi@masbit.eu>
2025-01-30 19:14:07 +0200

    feature: used effect for ai-chat

--- ws-server/utils/gemini-api.js
+++ ws-server/utils/gemini-api.js
@@ -7,8 +7,6 @@ export async function getGeminiResponse(prompt) {
     throw new Error("API_KEY is missing. Set it in your environment variables.");
   }
 
-  console.log(API_KEY);
-  // const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
   const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
   const requestData = {
@@ -31,11 +29,4 @@ export async function getGeminiResponse(prompt) {
     console.error("Gemini API error:", error.response?.data ?? error.message);
     throw new Error("Failed to fetch response from Gemini API.");
   }
-
-  // const response = await axios.post(url, {
-  //   prompt: { text: prompt },
-  //   temperature: 0.7,
-  // });
-  //
-  // return response.data.candidates[0].output;
 }
